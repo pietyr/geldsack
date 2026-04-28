56b7ddf50720b15c6184aab9ead161fcfac10568
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2026-04-28 13:17:29 +0200

    docs: generate automated model PHPDoc

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -8,6 +8,25 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property \Carbon\CarbonImmutable|null $created_at
+ * @property \Carbon\CarbonImmutable|null $updated_at
+ * @property int $user_id
+ * @property numeric $starting_balance
+ * @property string $name
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Wallet newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Wallet newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Wallet query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Wallet whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Wallet whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Wallet whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Wallet whereStartingBalance($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Wallet whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Wallet whereUserId($value)
+ * @mixin \Eloquent
+ */
 #[Fillable(['name', 'starting_balance', 'user_id'])]
 class Wallet extends Model
 {
